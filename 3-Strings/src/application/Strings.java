e2df85597da633f5549b7f925c7a1bab8ea1cc27
sathucandy <34880283+sathucandy@users.noreply.github.com>
2019-03-29 18:11:32 +0530

    Add files via upload

---
+++ 3-Strings/src/application/Strings.java
@@ -0,0 +1,28 @@
+package application;
+
+public class Strings {
+
+	public static void main(String[] args) {
+		int myInt = 7;
+
+		String text = "Hello";
+
+		String blank = " ";
+
+		String name = "Bob";
+
+		String greeting = text + blank + name;
+
+		System.out.println(greeting);
+
+		System.out.println("Hello" + " " + "Bob");
+
+		System.out.println("My integer is: " + myInt);
+
+		double myDouble = 7.8;
+
+		System.out.println("My number is: " + myDouble + ".");
+
+	}
+
+}
