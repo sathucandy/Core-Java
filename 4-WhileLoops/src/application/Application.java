6dc8b86804f45bb5907dfd6d1c016a47b81377f3
sathucandy <34880283+sathucandy@users.noreply.github.com>
2019-03-29 18:12:57 +0530

    Add files via upload

---
+++ 4-WhileLoops/src/application/Application.java
@@ -0,0 +1,16 @@
+package application;
+
+public class Application {
+
+	public static void main(String[] args) {
+		int value = 0;
+
+		while (value < 10) {
+			System.out.println("Hello " + value);
+
+			value = value + 1;
+		}
+
+	}
+
+}
